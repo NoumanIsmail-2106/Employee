0f56ad88e378b1086e1c042e79f97e169063a09a
NoumanIsmail <nomanismail2106a@aptechorangi.com>
2023-08-31 14:30:49 +0500

    Add project files.

---
+++ BackEnd/BackEnd/Models/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Models
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set;}
+    }
+}
